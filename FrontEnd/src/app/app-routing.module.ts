1b4d581a41e92bda07d7f39a857c453853102c63
Ambili001 <105069520+Ambili001@users.noreply.github.com>
2022-11-27 12:43:00 +0530

    Faculty home added

--- FrontEnd/src/app/app-routing.module.ts
+++ FrontEnd/src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
-
+import { FacultyHomeComponent } from './pages/faculty-home/faculty-home.component';
 const routes: Routes = [{path:' ',component:HomeComponent}];
 
 @NgModule({
--- FrontEnd/src/app/app.module.ts
+++ FrontEnd/src/app/app.module.ts
@@ -4,17 +4,27 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
-
-
+import { FacultyHomeComponent } from './pages/faculty-home/faculty-home.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { RouterModule ,Routes } from '@angular/router';
+import { FooterComponent } from './components/footer/footer.component';
+const appRoutes:Routes=[
+  {path:"",component:HomeComponent},
+  {path:"faculty-home",component:FacultyHomeComponent}
+]
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    FacultyHomeComponent,
+    NavbarComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
