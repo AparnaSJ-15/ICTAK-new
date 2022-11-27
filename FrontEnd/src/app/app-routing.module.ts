5b04b771fe97bcb8778ff8e6f78ba4af8b586685
mayachandrasekharan <112247016+mayachandrasekharan@users.noreply.github.com>
2022-11-27 20:18:56 +0530

    faculty login

--- FrontEnd/src/app/app-routing.module.ts
+++ FrontEnd/src/app/app-routing.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
 import { FacloginComponent } from './pages/faclogin/faclogin.component';
 
 import { HomeComponent } from './pages/home/home.component';
 
 const routes: Routes = [{path:'home',component:HomeComponent},
-                        {path:'faclogin',component:FacloginComponent}
+                        {path:'faclogin',component:FacloginComponent},
+                        {path:'header',component:HeaderComponent},
+                        {path:'footer',component:FooterComponent}
                       ];
 
 @NgModule({
