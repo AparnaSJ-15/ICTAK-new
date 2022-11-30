7a511c6b0634e426ef9f59f06d5f2707d4eefa9e
Aparna Suresh <aparnasuresh0002@gmail.com>
2022-11-30 10:33:02 +0530

    commit

--- FrontEnd/src/app/app.module.ts
+++ FrontEnd/src/app/app.module.ts
@@ -6,26 +6,20 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
 import { RouterModule ,Routes } from '@angular/router';
+import { AdminLoginComponent } from './pages/admin-login';
 
-import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
-import { FacloginComponent } from './pages/faclogin/faclogin.component';
-import { AdminLoginComponent } from './pages/admin-login/admin-login.component';
-
-
+const appRoutes=[{path:"",component:HomeComponent}]
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    HeaderComponent,
-    FooterComponent,
-    FacloginComponent,
     AdminLoginComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
